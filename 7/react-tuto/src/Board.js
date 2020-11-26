01f649443cb472fa81e59038ede1df944dfa49bc
Pierre Segalen <pierre.segalen@gmail.com>
2020-11-26 17:47:17 +0100

    Course 7 - Exercise finished

--- 7/react-tuto/src/Board.js
+++ 7/react-tuto/src/Board.js
@@ -1,12 +1,54 @@
 import React from "react";
 import Square from "./Square";
 
-const Board = () => {
-  const status = "Next player: X";
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a];
+    }
+  }
+  return null;
+};
 
-  const renderSquare = (i) => {
-    return <Square />;
+const Board = () => {
+  const [squares, setSquares] = React.useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = React.useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner !== null) {
+    status = `${winner} won!`;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
+  const handleClick = (i) => {
+    if (!calculateWinner(squares) && squares[i] === null) {
+      const newSquares = squares.slice();
+      newSquares[i] = xIsNext ? "X" : "O";
+      setSquares(newSquares);
+      setXIsNext(!xIsNext);
+    }
   };
+
+  const renderSquare = (i) => (
+    <Square value={squares[i]} onClick={() => handleClick(i)} />
+  );
 
   return (
     <div>
